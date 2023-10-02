a60ac52946176205168fbfd5e800c36c1c571bd5
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2023-10-02 19:57:49 +0530

    Full project code

---
+++ app.js
@@ -0,0 +1,44 @@
+// app.js file
+
+// code for fetch
+let ProjectList;
+const projectBody = document.querySelector(".projects");
+
+fetch("./contribution/ProjectList.json")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    ProjectList = data;
+    console.log({ ProjectList });
+    // code for cards creation on homepage
+    ProjectList.map((project) => {
+      const card = document.createElement("a");
+      card.classList.add("card");
+      card.setAttribute("href", project.FilePath);
+      const h3 = document.createElement("h3");
+      const p = document.createElement("p");
+      const tags = document.createElement("div");
+      tags.classList.add("tags");
+      project.tags.forEach((ele) => {
+        var tag = document.createElement("span");
+        tag.innerHTML = ele;
+        tag.classList.add("tag");
+        tags.appendChild(tag);
+      });
+      const author=document.createElement("a");
+      author.innerHTML="Author: "+project.Author;
+      author.setAttribute("href",project.Github);
+      h3.innerText = project.Name;
+      p.innerHTML = project.Description;
+      card.appendChild(h3);
+      card.appendChild(tags);
+      card.appendChild(author);
+      card.appendChild(p);
+      projectBody.appendChild(card);
+    });
+  })
+  .catch((error) => console.error("Error loading JSON file:", error));
