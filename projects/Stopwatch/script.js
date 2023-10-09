42f3fdd98cc7e44e72742bf4425e17cf4f1c3389
Aaryash-Shakya <100826663+Aaryash-Shakya@users.noreply.github.com>
2023-10-09 18:21:15 +0545

    add: changed variable name to be more readable AND add: comment

--- projects/Stopwatch/script.js
+++ projects/Stopwatch/script.js
@@ -1,8 +1,8 @@
 // get the necessary elements using DOM
-const hrDisplay = document.getElementById('hrClock')
-const minDisplay = document.getElementById('minClock')
-const secDisplay = document.getElementById('secClock')
-const msDisplay = document.getElementById('msClock')
+const hourDisplay = document.getElementById('hourClock')
+const minuteDisplay = document.getElementById('minuteClock')
+const secondDisplay = document.getElementById('secondClock')
+const millisecondDisplay = document.getElementById('millisecondClock')
 
 const lapBox = document.getElementById('lapBox')
 
@@ -12,7 +12,7 @@ const lapBtn = document.getElementById('lapBtn')
 const resetBtn = document.getElementById('resetBtn')
 
 // initialize
-let hr = 0, min = 0, sec = 0, ms = 0
+let hour = 0, minute = 0, second = 0, millisecond = 0
 let clockActive = false
 let lapIndex = 1, lapScore = ''
 
@@ -25,55 +25,64 @@ const getTwoDigit = (val) => {
     return val
 }
 
+// function to increment the clock
 const stopwatch = () => {
     if (clockActive) {
-        if (++ms == 100) {
-            ms = 0
-            if (++sec == 60) {
-                sec = 0
-                if (++min == 60) {
-                    min = 0
-                    hr++
-                    hrDisplay.innerText = getTwoDigit(hr)
+        // increment millisecond.
+        if (++millisecond == 100) {
+            // if millisecond == 100 then reset millisecond and increment second
+            millisecond = 0
+            if (++second == 60) {
+                // if second == 60 then reset second and increment minute
+                second = 0
+                if (++minute == 60) {
+                    // if minute == 60 then reset minute and increment hour
+                    minute = 0
+                    hour++
+                    hourDisplay.innerText = getTwoDigit(hour)
                 }
-                minDisplay.innerText = getTwoDigit(min)
+                minuteDisplay.innerText = getTwoDigit(minute)
             }
-            secDisplay.innerText = getTwoDigit(sec)
+            secondDisplay.innerText = getTwoDigit(second)
         }
-        msDisplay.innerText = getTwoDigit(ms)
+        millisecondDisplay.innerText = getTwoDigit(millisecond)
 
-        // call stopwatch() every 10ms
+        // call stopwatch() every 10millisecond
         setTimeout(stopwatch, 10)
     }
 }
 
+// on clicking start set clockActive = true
 startBtn.onclick = () => {
     clockActive = true
     stopwatch()
 }
 
+// on clicking stop set clockActive = false
 stopBtn.onclick = () => {
     clockActive = false;
 }
 
+// on clicking lap append the lab record in variable and export to HTML
 lapBtn.onclick = () => {
     if (lapIndex > 10) {
         return alert('Maximum lap record reached')
     }
     // append new lap record and export to HTML
-    lapScore += `<p>${lapIndex++}.<span class="margin-x">${getTwoDigit(hr)} : ${getTwoDigit(min)} : ${getTwoDigit(sec)} : ${getTwoDigit(ms)}</span></p>`
+    lapScore += `<p>${lapIndex++}.<span class="margin-x">${getTwoDigit(hour)} : ${getTwoDigit(minute)} : ${getTwoDigit(second)} : ${getTwoDigit(millisecond)}</span></p>`
     lapBox.innerHTML = lapScore
 }
 
+// on clicking reset set clockActive = false and reset all variables to 0 and lap record to blank
 resetBtn.onclick = () => {
     clockActive = false;
 
     // reset variables to 0
-    hr = 0, min = 0, sec = 0, ms = 0
-    hrDisplay.innerText = getTwoDigit(hr)
-    minDisplay.innerText = getTwoDigit(min)
-    secDisplay.innerText = getTwoDigit(sec)
-    msDisplay.innerText = getTwoDigit(ms)
+    hour = 0, minute = 0, second = 0, millisecond = 0
+    hourDisplay.innerText = getTwoDigit(hour)
+    minuteDisplay.innerText = getTwoDigit(minute)
+    secondDisplay.innerText = getTwoDigit(second)
+    millisecondDisplay.innerText = getTwoDigit(millisecond)
 
     // reset lap
     lapIndex = 1
