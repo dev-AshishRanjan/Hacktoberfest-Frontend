2b17bd57bddc58edf1e786a5223940af1b28e6b5
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2024-01-06 16:00:20 +0530

    FIX : footer null

--- app.js
+++ app.js
@@ -210,7 +210,7 @@ if (window.location.pathname === "/contributors.html") {
 
 // footer
 const footer = document.querySelector(".footer");
-footer !== undefined
+footer !== null
   ? (footer.innerHTML = `
 <p>Open Source Project © <span>${new Date().getFullYear()}</span></p>
           <p>Developed by <a href="https://github.com/dev-AshishRanjan"><span>Kumar Ashish Ranjan</span></a> and <a href="/contributors.html"><span>Open Source Community</span></a></p>
