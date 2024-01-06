76c8a46171d5ac994cd75728f711e73da4438f50
dev-AshishRanjan <94990764+dev-AshishRanjan@users.noreply.github.com>
2024-01-06 15:08:23 +0530

    ADD : searchbar in contributionpage

--- app.js
+++ app.js
@@ -21,93 +21,97 @@ function breakdown(list) {
 let ProjectList;
 const projectBody = document.querySelector(".projects");
 const searchTerm = window.location.search.split("=")[1] || "";
-fetch("./contribution/ProjectList.json")
-  .then((response) => {
-    if (!response.ok) {
-      return (ProjectList = [
-        {
-          Name: `Status: ${response.status}`,
-          Author: "HTTP",
-          tags: [`${response.status}`],
-          Github: "",
-          FilePath: "#",
-          Description: "Use console to know this error better",
-        },
-      ]);
-      // throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-    return response.json();
-  })
-  .then((data) => {
-    // ProjectList = data;
-    console.log({ searchTerm });
-    console.log({ data });
-    ProjectList = data.filter((ele) => {
-      return (
-        ele.Name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        ele.Author.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        ele.tags.includes(searchTerm.toLowerCase())
+if (window.location.pathname === "/") {
+  fetch("./contribution/ProjectList.json")
+    .then((response) => {
+      if (!response.ok) {
+        return (ProjectList = [
+          {
+            Name: `Status: ${response.status}`,
+            Author: "HTTP",
+            tags: [`${response.status}`],
+            Github: "",
+            FilePath: "#",
+            Description: "Use console to know this error better",
+          },
+        ]);
+        // throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      // ProjectList = data;
+      console.log({ searchTerm });
+      console.log({ data });
+      ProjectList = data.filter((ele) => {
+        return (
+          ele.Name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          ele.Author.toLowerCase().includes(searchTerm.toLowerCase()) ||
+          ele.tags.includes(searchTerm.toLowerCase())
+        );
+      });
+      const SearchResultMetaData = document.querySelector(
+        ".SearchResultMetaData"
       );
-    });
-    const SearchResultMetaData = document.querySelector(
-      ".SearchResultMetaData"
-    );
-    searchTerm===''?SearchResultMetaData.innerHTML = `Total : <span>${ProjectList.length}</span>`:SearchResultMetaData.innerHTML = `<span>${ProjectList.length}</span> Match found for <span>${searchTerm}</span>`;
-    if (ProjectList.length == 0) {
-      ProjectList = [
-        {
-          Name: `No Results found for ${searchTerm}`,
-          Author: "search",
-          tags: [`error`],
-          Github: "",
-          FilePath: "#",
-          Description: "Use console to know this error better",
-        },
-      ];
-      SearchResultMetaData.innerHTML = `<span>No</span> Match found for <span>${searchTerm}</span>`;
-    }
-    ProjectList.sort(compareByName);
-    breakdown(ProjectList);
-    console.log({ ProjectList });
-    // console.log({ breakdownProjectList });
-    // code for cards creation on homepage
-    breakdownProjectList.map((groupData) => {
-      const group = document.createElement("div");
-      group.classList.add("group");
-      const groupheader = document.createElement("h2");
-      groupheader.classList.add("groupheader");
-      groupheader.innerText = groupData.char + " ---";
-      group.appendChild(groupheader);
-      groupData.data &&
-        groupData.data.map((project) => {
-          const card = document.createElement("a");
-          card.classList.add("card");
-          card.setAttribute("href", project.FilePath);
-          const h3 = document.createElement("h3");
-          const p = document.createElement("p");
-          const tags = document.createElement("div");
-          tags.classList.add("tags");
-          project.tags.forEach((ele) => {
-            var tag = document.createElement("span");
-            tag.innerHTML = ele;
-            tag.classList.add("tag");
-            tags.appendChild(tag);
+      searchTerm === ""
+        ? (SearchResultMetaData.innerHTML = `Total : <span>${ProjectList.length}</span>`)
+        : (SearchResultMetaData.innerHTML = `<span>${ProjectList.length}</span> Match found for <span>${searchTerm}</span>`);
+      if (ProjectList.length == 0) {
+        ProjectList = [
+          {
+            Name: `No Results found for ${searchTerm}`,
+            Author: "search",
+            tags: [`error`],
+            Github: "",
+            FilePath: "#",
+            Description: "Use console to know this error better",
+          },
+        ];
+        SearchResultMetaData.innerHTML = `<span>No</span> Match found for <span>${searchTerm}</span>`;
+      }
+      ProjectList.sort(compareByName);
+      breakdown(ProjectList);
+      console.log({ ProjectList });
+      // console.log({ breakdownProjectList });
+      // code for cards creation on homepage
+      breakdownProjectList.map((groupData) => {
+        const group = document.createElement("div");
+        group.classList.add("group");
+        const groupheader = document.createElement("h2");
+        groupheader.classList.add("groupheader");
+        groupheader.innerText = groupData.char + " ---";
+        group.appendChild(groupheader);
+        groupData.data &&
+          groupData.data.map((project) => {
+            const card = document.createElement("a");
+            card.classList.add("card");
+            card.setAttribute("href", project.FilePath);
+            const h3 = document.createElement("h3");
+            const p = document.createElement("p");
+            const tags = document.createElement("div");
+            tags.classList.add("tags");
+            project.tags.forEach((ele) => {
+              var tag = document.createElement("span");
+              tag.innerHTML = ele;
+              tag.classList.add("tag");
+              tags.appendChild(tag);
+            });
+            const author = document.createElement("a");
+            author.innerHTML = "Author: " + project.Author;
+            author.setAttribute("href", project.Github);
+            h3.innerText = project.Name;
+            p.innerHTML = project.Description;
+            card.appendChild(h3);
+            card.appendChild(tags);
+            card.appendChild(author);
+            card.appendChild(p);
+            group.appendChild(card);
           });
-          const author = document.createElement("a");
-          author.innerHTML = "Author: " + project.Author;
-          author.setAttribute("href", project.Github);
-          h3.innerText = project.Name;
-          p.innerHTML = project.Description;
-          card.appendChild(h3);
-          card.appendChild(tags);
-          card.appendChild(author);
-          card.appendChild(p);
-          group.appendChild(card);
-        });
-      projectBody.appendChild(group);
-    });
-  })
-  .catch((error) => console.error("Error loading JSON file:", error));
+        projectBody.appendChild(group);
+      });
+    })
+    .catch((error) => console.error("Error loading JSON file:", error));
+}
 
 // theme selection
 const html = document.documentElement;
@@ -151,21 +155,54 @@ function generateCard(ele) {
   `;
   contributors.appendChild(card);
 }
-
-try {
-  fetch(
-    "https://api.github.com/repos/dev-AshishRanjan/Hacktoberfest-Frontend/contributors?per_page=100"
-  )
-    .then((req) => req.json())
-    .then((res) => {
-      res.map((ele) => {
-        generateCard(ele);
+if (window.location.pathname === "/contributors.html") {
+  const SearchResultMetaData = document.querySelector(".SearchResultMetaData");
+  try {
+    fetch(
+      "https://api.github.com/repos/dev-AshishRanjan/Hacktoberfest-Frontend/contributors?per_page=100"
+    )
+      .then((req) => req.json())
+      .then((res) => {
+        resList = res.filter((ele) => {
+          return (
+            ele.login.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            JSON.stringify(ele.contributions).includes(searchTerm)
+          );
+        });
+        console.log({ resList });
+        searchTerm === ""
+          ? (SearchResultMetaData.innerHTML = `Total : <span>${resList.length}</span>`)
+          : (SearchResultMetaData.innerHTML = `<span>${resList.length}</span> Match found for <span>${searchTerm}</span>`);
+        if (resList.length === 0) {
+          resList = [
+            {
+              avatar_url: "https://avatars.githubusercontent.com/u/94990764?v=4",
+              login: "No Match Found",
+              contributions: 0,
+            },
+          ];
+          SearchResultMetaData.innerHTML = `<span>No</span> Match found for <span>${searchTerm}</span>`;
+        }
+        resList.map((ele) => {
+          generateCard(ele);
+        });
+        console.log({ res });
+      })
+      .catch((e) => {
+        console.log({ e });
+        resList = [
+          {
+            avatar_url: "https://avatars.githubusercontent.com/u/94990764?v=4",
+            login: "404",
+            contributions: 0,
+          },
+        ];
+        SearchResultMetaData.innerHTML = `<span>Network</span> Error`;
+        resList.map((ele) => {
+          generateCard(ele);
+        });
       });
-      console.log({ res });
-    })
-    .catch((e) => {
-      contributors.innerText = "Network Error";
-    });
-} catch (e) {
-  contributors.innerText = "Network Error";
+  } catch (e) {
+    console.log({ e });
+  }
 }
